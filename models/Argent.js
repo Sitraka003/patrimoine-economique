3b30b502b365688750095d8bc1e2165b16d18474
JenniferTantely <hei.jennifer.2@gmail.com>
2024-07-19 20:30:39 +0300

    feat: Add three class that extends Possession

---
+++ models/Argent.js
@@ -0,0 +1,13 @@
+class Argent extends Possession {
+    constructor(possesseur, libelle, montant) {
+      super(possesseur, "argent", libelle);
+      this.montant = montant;
+    }
+  
+    getValeur(date) {
+      return this.montant;
+    }
+}
+
+
+module.exports = Argent;
