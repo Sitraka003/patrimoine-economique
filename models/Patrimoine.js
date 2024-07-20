9a4d30f06777b7b118cb5cd71e2cf203da4bf395
Phylosius <hei.phylosius@gmail.com>
2024-07-20 10:39:27 +0300

    feat: created getValeur method for Patrimoine

--- models/Patrimoine.js
+++ models/Patrimoine.js
@@ -1,21 +1,51 @@
 /**
  * Classe qui represente une patrimoine d'une personne ( ensemble de possessions )
  */
-class Patrimoine {
-  constructor(possesseur, date, possessions) {
+export class Patrimoine {
+  /**
+   * @param possesseur {Personne}
+   * @param date {String}
+   * @param possessions {Possession[]}
+   * */
+  constructor(possesseur, date, possessions=[]) {
     this.possesseur  = possesseur;
-    this.date = date
+    this.date = new Date(date);
+
     this.possessions = possessions; // [Possession, Possession, ...]
   }
-  getValeur(date) {
-    return 
+
+  /**
+   * retourne la valeur total de tout les patrimoine à une date donnée
+   *
+   * @param _date {String} Date de la valeur qu'on veut
+   * @param rounded {boolean} indique si on veut une valuer arrondie ou non
+   * @param roundEach {boolean} indique si on veut arrondir la valeur de chaque possession
+   * */
+  getValeur(_date, rounded=false, roundEach=false) {
+    let result = 0;
+    this.possessions.forEach(possession => {
+      result += possession.getValeur(_date, roundEach);
+    })
+
+    return rounded ? Math.round(result) : result;
   }
+
+  /**
+   * Ajoute une possession au patrimoine
+   *
+   * @param possession {Possession} possession à ajouter
+   * */
   addPossession(possession) {
     this.possessions.push(possession);
   }
+
+  /**
+   * Enleve une possession au patrimoine
+   *
+   * @param possession {Possession} possession à enlever
+   * */
   removePossession(possession) {
     this.possessions = this.possessions.filter(p => p.libelle !== possession.libelle);
   }
 }
 
-module.exports = Patrimoine;
