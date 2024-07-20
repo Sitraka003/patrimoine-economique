c7e2b34f3cb3e5e56dcec7a0afac63f14b18ff65
NONIE_KELY <154633311+nonieKely@users.noreply.github.com>
2024-07-20 11:08:47 +0300

    feat : add showPossession and get NewValue function

--- models/Possession.js
+++ models/Possession.js
@@ -1,8 +1,28 @@
 class Possession {
-  constructor(possesseur, type, libelle) {
+  constructor(possesseur, type, libelle,valeur,tauxAmortissement,dateAchat,dateVente) {
     this.possesseur = possesseur;
     this.type = type;
     this.libelle = libelle;
+    this.valeur = valeur;
+    this.tauxAmortissement = tauxAmortissement;
+    this.dateAchat = dateAchat;
+    this.dateVente = dateVente;
+    
   }
+  
+
+  showPossession(possession){
+    console.log('Prorpriétaire : ' + this.possesseur + "\n type de produit : " + this.type + " \n valeur :" + this.valeur)
+  }
+
+  getNewValue(possession){
+    let today = new Date()
+    const newDate = today.getFullYear -this.dateAchat ;
+      let lostValue= (10/100)*this.valeur ;
+      let newValue = this.valeur - newDate*lostValue
+
+      return newValue;
+
+    }
 }
 module.exports = Possession;
