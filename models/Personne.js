7f44e6d6f7f155f810ac9d15fed8336c161229d4
NONIE_KELY <154633311+nonieKely@users.noreply.github.com>
2024-07-20 11:07:54 +0300

    feat : add showUser function

--- models/Personne.js
+++ models/Personne.js
@@ -1,7 +1,15 @@
 class Personne {
-  constructor(nom) {
+  constructor(nom,prenom,dateDeNaissance) {
     this.nom = nom;
+    this.prénom = prenom;
+    this.dateDeNaissance = dateDeNaissance;
   }
+
+  showUser(user) {
+    console.log("mon nom est " + this.nom + this.prenom + "la date de naisssance est " + this.dateDeNaissance)
+  }
+
+  
 }
 
-module.exports = Person;
+module.exports = Personne;
