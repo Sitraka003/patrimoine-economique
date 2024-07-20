a76b4023fa79c6b68d93d0b874197f665f3bcf44
JenniferTantely <hei.jennifer.2@gmail.com>
2024-07-20 17:26:02 +0300

    feat: Add Main.js to test all the models

---
+++ models/Main.js
@@ -0,0 +1,32 @@
+const Personne = require('./Personne');
+const Argent = require('./Argent');
+const BienMateriel = require('./BienMateriel');
+const TrainDeVie = require('./TrainDeVie');
+const Patrimoine = require('./Patrimoine');
+
+// Création de la personne Ilo
+const ilo = new Personne('Ilo');
+
+// Création des possessions
+const especes = new Argent(ilo, 'especes', 400000);
+const compteEpargne = new Argent(ilo, 'compte_epargne', 20000, '2024-01-01', 0, 0, '2023-06-01', 5);
+//salaire : 600 000ar
+const compteCourant = new Argent(ilo, 'compte_courant', 0, '2024-01-01', 600000, 500000);
+const ordinateur = new BienMateriel(ilo, 'ordinateur', 2000000, '2021-10-26', 10);
+const effetsVestimentaires = new BienMateriel(ilo, 'effets_vestimentaires', 1000000, '2024-01-01', 20);
+const trainDeVie = new TrainDeVie(ilo, 'train_de_vie', 500000);
+
+// Création du patrimoine
+const patrimoine = new Patrimoine(ilo, '2024-05-13', [especes, compteEpargne, compteCourant, ordinateur, effetsVestimentaires, trainDeVie]);
+
+const valeur26Juin2024 = patrimoine.getValeur('2024-06-26');
+console.log(`Valeur du patrimoine le 26 juin 2024 : ${valeur26Juin2024} Ar`);
+
+const valeur14Juillet2024 = patrimoine.getValeur('2024-07-14');
+console.log(`Valeur du patrimoine le 14 juillet 2024 : ${valeur14Juillet2024} Ar`);
+
+const montantCompteCourant14Juillet = compteCourant.getValeur('2024-07-14');
+console.log(`Montant sur le compte courant le 14 juillet 2024 : ${montantCompteCourant14Juillet} Ar`);
+
+const montantCompteEpargne14Juillet = compteEpargne.getValeur('2024-07-14');
+console.log(`Montant sur le compte courant le 14 juillet 2024 : ${montantCompteEpargne14Juillet} Ar`);
