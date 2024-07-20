31cb1e54663f9093835d893a657a2985141d05f5
JenniferTantely <hei.jennifer.2@gmail.com>
2024-07-20 16:09:11 +0300

    feat: Add the implementation of getValeur Argent if the type of libelle is 'compte_courant'

--- models/Argent.js
+++ models/Argent.js
@@ -1,26 +1,36 @@
 class Argent extends Possession {
-    constructor(possesseur, libelle, montant) {
-      super(possesseur, "argent", libelle);
-      this.montant = montant;
-    }
-  
-    getValeur(date) {
-      const dateActuelle = new Date(date);
+  constructor(possesseur, libelle, montant, dateCreation = null, salaireMensuel = 0, trainDeVieMensuel = 0) {
+    super(possesseur, "argent", libelle);
+    this.montant = montant;
+    this.dateCreation = dateCreation ? new Date(dateCreation) : null;
+    this.salaireMensuel = salaireMensuel;
+    this.trainDeVieMensuel = trainDeVieMensuel;
+  }
 
-      switch (this.libelle) {
-        case 'especes': {
-          return this.montant;
-        }
-        case 'compte_courant': {
+  getValeur(date) {
+    const dateActuelle = new Date(date);
 
-        }
-        case 'compte_epargne': {
-
-        }
-        default:
-          return 0;
+    switch (this.libelle) {
+      case 'especes': {
+        return this.montant;
       }
+      case 'compte_courant': {
+        const moisEcoules = (dateActuelle.getFullYear() - this.dateCreation.getFullYear()) * 12 + (dateActuelle.getMonth() - this.dateCreation.getMonth());
+        let valeurActuelle = this.montant;
+
+        for (let i = 0; i < moisEcoules; i++) {
+          valeurActuelle += (this.salaireMensuel - this.trainDeVieMensuel);
+        }
+
+        return valeurActuelle;
+      }
+      case 'compte_epargne': {
+
+      }
+      default:
+        return 0;
     }
+  }
 }
 
 
