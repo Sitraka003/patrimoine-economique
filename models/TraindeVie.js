5ed33674c908f7eb6aee53eaacc027443ab82b77
Ice_dera3 <hei.sedera@gmail.com>
2024-07-26 20:01:04 +0300

    feat: TrainDeVie.js creation

---
+++ models/TraindeVie.js
@@ -0,0 +1,24 @@
+class Possesseur {
+    constructor() {
+        this.possessions = [];
+    }
+
+    ajouterPossession(possession) {
+        this.possessions.push(possession);
+    }
+
+    getPossession(type) {
+        return this.possessions.find(p => p.type === type);
+    }
+
+    payerFrais(trainDeVie) {
+        const argent = this.getPossession("argent");
+        if (argent && argent.montant >= trainDeVie.coutMensuel) {
+            argent.montant -= trainDeVie.coutMensuel;
+            return true;
+        }
+        return false;
+    }
+}
+
+export default Possesseur;
