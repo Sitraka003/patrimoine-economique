23ca4d52cc0f16e2b638ae9bb4dc98f2fe01dd62
Abega1642 <hei.abega.2@gmail.com>
2024-07-26 16:56:03 +0300

    feat: add some missing fields & add getValue method

--- models/Possession.js
+++ models/Possession.js
@@ -1,8 +1,30 @@
 class Possession {
-  constructor(possesseur, type, libelle) {
+  constructor(possesseur, libelle, nom, valeur, dateAchat, dateExpiration, tauxAmortissement) {
     this.possesseur = possesseur;
-    this.type = type;
     this.libelle = libelle;
+    this.name = nom;
+    this.valeur = valeur;
+    this.type = type;
+    this.dateAchat = dateAchat;
+    this.dateExpiration = dateExpiration;
+    this.tauxAmortissement = tauxAmortissement;
+  }
+
+  getValueAt(dateEvaluation) {
+
+    let dateDeCommencement = moment(this.buyingDate, 'YYYY-MM-DD');
+    let dateDeFin = moment(dateEvaluation, 'YYYY-MM-DD');
+
+    let difference = moment.duration(dateDeFin.diff(dateDeCommencement));
+    let annee = diff.years();
+    let mois = diff.months();
+    let jour = diff.days();
+
+    let raison = annee + mois / 12 + jour / 365;
+
+    this.valeur -= this.tauxAmortissement * this.value * (ratio / 100);
+
+    return this.value;
   }
 }
 module.exports = Possession;
