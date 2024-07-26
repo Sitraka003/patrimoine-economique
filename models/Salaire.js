10693104ed842fde75d791667a6ee2ab3bdf1377
Abega1642 <hei.abega.2@gmail.com>
2024-07-26 20:54:33 +0300

    fix: fixed getValueAt method in models/Salaire.js

--- models/Salaire.js
+++ models/Salaire.js
@@ -1,20 +1,26 @@
-import {Possession} from './Possession'
+import { Possession } from './Possession.js'
 import moment from "moment";
 
 class Salaire extends Possession {
-    constructor(possesseur, libelle, nom, valeur, dateAchat, dateExpiration, tauxAmortissement) {
-        super(possesseur, libelle, nom, valeur, dateAchat, dateExpiration, tauxAmortissement),
+    constructor(possesseur, libelle, nom, valeur, dateAchat, dateExpiration) {
+        super(possesseur, libelle, nom, valeur, dateAchat, dateExpiration, 0);
         this.valeurConstante = valeur;
     }
     
     getValueAt(dateEvaluation) {
-        dateEvaluation = moment(dateEvaluation, 'YYYY-MM-DD');
-        const finDuMois = date.clone().endOf('month');
+        let commencement = moment(this.dateAchat).startOf('day');
+        const fin = moment(dateEvaluation).startOf('day');
 
-        if (estfinDuMois) {
-            this.value += this.constantValue * months;
-            console.log(` +++ "Salary" added successfully! +++ `);
+        while (commencement.isSameOrBefore(fin)) {
+            if (commencement.clone().endOf('month').isSame(commencement, 'day')) {
+                this.valeur += this.valeurConstante;
+            }
+            if (commencement.clone().startOf('month').isSame(commencement, 'day')) {
+                this.valeur -= 50; // Arbitraire
+            }
+            commencement.add(1, 'day');
         }
-
+        return this.valeur
     }
 }
+
