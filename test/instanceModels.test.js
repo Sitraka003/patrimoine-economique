d8e1b511c806d50e0445e98ea4738c1ebd991ee5
JenniferTantely <hei.jennifer.2@gmail.com>
2024-07-28 22:31:40 +0300

    fix: make the instantiation test more clear

--- test/instanceModels.test.js
+++ test/instanceModels.test.js
@@ -7,57 +7,41 @@ import { BienMateriel } from "../models/typeOfPossessions/BienMateriel.js";
 import { TrainDeVie } from "../models/typeOfPossessions/TrainDeVIe.js";
 import { Patrimoine } from "../models/Patrimoine.js";
 
-describe("tester la classe Personne", () => {
+describe("Tester l'instanciation de toutes les classes", () => {
     it("devrait créer une instance de Personne", () => {
-        const rakoto = new Personne("Rakoto")
-        assert.equal("Rakoto", rakoto.nom)
-    })
-});
+        const rakoto = new Personne("Rakoto");
+        assert.equal("Rakoto", rakoto.nom);
+    });
 
-describe("tester la classe Possessions", () => {
-    it("devrait créer une instance de Possessions", () => {
+    it("devrait créer une instance de Possession", () => {
         const rakoto = new Personne("Rakoto");
         const ordinateur = new Possession(rakoto, 'bien_materiel', 'ordinateur');
-        assert.equal("ordinateur", ordinateur.libelle)
+        assert.equal("ordinateur", ordinateur.libelle);
+    });
 
-    })
-});
-
-describe("tester la classe fille Argent", () => {
     it("devrait créer une instance de Argent", () => {
         const rakoto = new Personne("Rakoto");
         const especes = new Argent(rakoto, 'especes', 400000);
-        assert.equal(400000, especes.getValeur('2024-07-06'))
+        assert.equal(400000, especes.getValeur('2024-07-06'));
+    });
 
-    })
-});
-
-describe("tester la classe fille BienMateriel", () => {
     it("devrait créer une instance de BienMateriel", () => {
         const rakoto = new Personne("Rakoto");
         const ordinateur = new BienMateriel(rakoto, 'ordinateur', 2000000, '2021-10-26', 10);
         assert.equal(2000000, ordinateur.valeurInitiale);
+    });
 
-    })
-});
-
-describe("tester la classe fille TrainDeVie", () => {
     it("devrait créer une instance de TrainDeVie", () => {
         const rakoto = new Personne("Rakoto");
         const trainDeVie = new TrainDeVie(rakoto, 'train_de_vie', 500000);
-        assert.equal(500000, trainDeVie.getValeur('2024-07-06'))
+        assert.equal(500000, trainDeVie.getValeur('2024-07-06'));
+    });
 
-    })
-});
-
-
-describe("tester la classe Patrimoine", () => {
     it("devrait créer une instance de Patrimoine", () => {
         const rakoto = new Personne("Rakoto");
         const trainDeVie = new TrainDeVie(rakoto, 'train_de_vie', 500000);
         const especes = new Argent(rakoto, 'especes', 400000);
         const patrimoine = new Patrimoine(rakoto, '2024-05-13', [especes, trainDeVie]);
-        assert.equal('2024-05-13', patrimoine.date)
-
-    })
-})
+        assert.equal('2024-05-13', patrimoine.date);
+    });
+});
