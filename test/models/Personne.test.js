585dac58831e3767b13e3a304100e689b4d20da2
Phylosius <hei.phylosius@gmail.com>
2024-07-31 10:59:13 +0300

    Created test for Personne

---
+++ test/models/Personne.test.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+
+import { Personne } from '../../models/Personne.js';
+
+describe('Personne tests', () => {
+    const johnName = "John Doe"
+    const john = new Personne(johnName)
+
+    it("creation d'une personne", () => {
+        expect(typeof john !== 'undefined').to.be.true;
+    })
+    it("integrité des attributs", () => {
+        expect(john.nom).to.equal(johnName)
+    })
+})
