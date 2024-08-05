cea43df3df85c10aa9d59c08ede19f425fd80192
AinaElie <elie.anjarafanomezana@gmail.com>
2024-08-05 09:38:02 +0300

    feat: affichage des possessions

--- ui/patrimoine_react/src/App.jsx
+++ ui/patrimoine_react/src/App.jsx
@@ -1,48 +1,35 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
-
 import React from 'react';
 // import { Button } from 'react-bootstrap';
 import data from '../../../data/data.json'
+import { Table } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, {data.nom} </h1>
+    <div className="container">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Valeur</th>
+            <th>Date de Debut</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{data.possessions.map(element => (
+              <p>{element.libelle}</p>
+            ))}</td>
+            <td>{data.possessions.map(element => (
+              <p>{Math.abs(element.valeur) || Math.abs(element.valeurConstante)}</p>
+            ))}</td>
+            <td>{data.possessions.map(element => (
+              <p>{element.dateDebut}</p>
+            ))}</td>
+          </tr>
+        </tbody>
+      </Table>
       {/* <Button variant="primary">Primary Button</Button> */}
+
     </div>
   );
 }
