a15ca4a29a008a3a43727239c38a60345b11e93d
AinaElie <elie.anjarafanomezana@gmail.com>
2024-08-08 09:01:42 +0300

    feat: concurently

--- ui/patrimoine_react/src/Components/Result.jsx
+++ ui/patrimoine_react/src/Components/Result.jsx
@@ -7,13 +7,13 @@ import BienMateriel from "../../../../models/possessions/BienMateriel.js";
 
 
 const ilo = new Personne("Ilo");
-const argent1 = new Argent(ilo.nom, "especes", 400_000, new Date("2024-5-13"), null, null, TYPE_ARGENT.Espece);
-const argent2 = new Argent(ilo.nom, "epargne", 200_000, new Date("2024-5-13"), null, null, TYPE_ARGENT.Epargne);
-const argent3 = new Argent(ilo.nom, "courant", -600_000, new Date("2024-5-13"), null, null, TYPE_ARGENT.Courant);
-const bienMateriel1 = new BienMateriel(ilo.nom, "Ordinateur", 2_000_000, new Date("2021-10-26"), null, 10);
-const bienMateriel2 = new BienMateriel(ilo.nom, "Effets vestimentaires", 1_000_000, new Date("2024-1-1"), null, 20);
-const trainDeVie = new Flux(ilo.nom, "train de vie", -500_000, new Date("2024-5-13"), null, null, 14);
-const salaire = new Flux(ilo.nom, "salaire", 200_000, new Date("2024-1-1"), null, null, 2);
+const argent1 = new Argent(ilo.nom, "Argent", 400_000, new Date(), null, null, TYPE_ARGENT.Espece);
+const argent2 = new Argent(ilo.nom, "Argent", 200_000, new Date(), null, null, TYPE_ARGENT.Epargne);
+const argent3 = new Argent(ilo.nom, "Argent", -600_000, new Date(), null, null, TYPE_ARGENT.Courant);
+const bienMateriel1 = new BienMateriel(ilo.nom, "Ordinateur", 2_000_000, new Date(), null, 10);
+const bienMateriel2 = new BienMateriel(ilo.nom, "Effets vestimentaires", 1_000_000, new Date(), null, 20);
+const trainDeVie = new Flux(ilo.nom, "train de vie", -500_000, new Date(), null, null, 14);
+const salaire = new Flux(ilo.nom, "salaire", 200_000, new Date(), null, null, 2);
 
 const patrimoine = new Patrimoine(ilo.nom, [argent1, argent2, argent3, bienMateriel1, bienMateriel2, trainDeVie, salaire]);
 
