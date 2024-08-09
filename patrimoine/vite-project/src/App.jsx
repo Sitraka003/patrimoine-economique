d3b0eff998c3609bf0fb551ce81fb3bbdbf64659
Ice_dera3 <hei.sedera@gmail.com>
2024-08-09 21:57:12 +0300

    feat: creating vite project

--- patrimoine/vite-project/src/App.jsx
+++ patrimoine/vite-project/src/App.jsx
@@ -1,35 +1,138 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import data from '../../../data/data.json';
+import Patrimoine from '../../../models/Patrimoine.js';
+import Possession from '../../../models/possessions/Possession.js';
+import Flux from "../../../models/possessions/Flux.js";
+import './App.css';
+
+function PossessionTable({ possessions }) {
+  const today = new Date(Date.now());
+
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr className='text-center'>
+          <th>#</th>
+          <th>Libelle</th>
+          <th>Valeur initiale (Ariary)</th>
+          <th>Date de début</th>
+          <th>Date de fin</th>
+          <th>Amortissement</th>
+          <th>Valeur Actuelle (Ariary)</th>
+        </tr>
+      </thead>
+      <tbody>
+        {possessions.map((item, index) => {
+          const possession = item.jour
+            ? new Flux(
+                item.possesseur,
+                item.libelle,
+                item.valeurConstante,
+                new Date(item.dateDebut),
+                item.dateFin ? new Date(item.dateFin) : null,
+                item.tauxAmortissement,
+                item.jour
+              )
+            : new Possession(
+                item.possesseur,
+                item.libelle,
+                item.valeur,
+                new Date(item.dateDebut),
+                item.dateFin ? new Date(item.dateFin) : null,
+                item.tauxAmortissement
+              );
+
+          const valeurActuelle = possession.getValeur(today);
+
+          return (
+            <tr key={`${item.libelle}-${index}`} className='text-center'>
+              <td>{index + 1}</td>
+              <td>{item.libelle}</td>
+              <td className='text-end'>{item.valeur}</td>
+              <td>{new Date(item.dateDebut).toLocaleDateString()}</td>
+              <td>{item.dateFin ? new Date(item.dateFin).toLocaleDateString() : "-"}</td>
+              <td>{item.tauxAmortissement ? item.tauxAmortissement : "-"}</td>
+              <td className='text-end'>{valeurActuelle.toFixed(2)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [patrimoineValue, setPatrimoineValue] = useState(0);
+
+  const calculatePatrimoineValue = () => {
+    if (selectedDate) {
+      try {
+        const possessions = data[1].data.possessions.map(item =>
+          item.jour
+            ? new Flux(
+                item.possesseur,
+                item.libelle,
+                item.valeurConstante,
+                new Date(item.dateDebut),
+                item.dateFin ? new Date(item.dateFin) : null,
+                item.tauxAmortissement,
+                item.jour
+              )
+            : new Possession(
+                item.possesseur,
+                item.libelle,
+                item.valeur,
+                new Date(item.dateDebut),
+                item.dateFin ? new Date(item.dateFin) : null,
+                item.tauxAmortissement
+              )
+        );
+
+        const patrimoine = new Patrimoine("John Doe", possessions);
+        const patValue = patrimoine.getValeur(selectedDate);
+        setPatrimoineValue(patValue);
+      } catch (error) {
+        console.error("Erreur lors du calcul du patrimoine : ", error);
+        alert("Une erreur est survenue lors du calcul du patrimoine.");
+      }
+    } else {
+      alert("VEUILLEZ TOUT D'ABORD SÉLECTIONNER UNE DATE\nMERCI");
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+        <div className='row justify-content-center'>
+          <div className='col-md-6'>
+            <h1 className='text-center bordered-title'>Patrimoine économique</h1>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <PossessionTable possessions={data[1].data.possessions} />
+      <div className='dateSelection'>
+        <label htmlFor="selectDate">Sélectionner une date :</label>
+        <DatePicker
+          selected={selectedDate}
+          onChange={(date) => setSelectedDate(date)}
+          dateFormat="dd/MM/yyyy"
+          placeholderText="La nouvelle date"
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='calcBtn'>
+        <Button variant="primary" onClick={calculatePatrimoineValue}>Calculer le patrimoine</Button>{' '}
+      </div>
+      <div className='calcResult'>
+        <p>Patrimoine du possesseur : </p><span className='text-danger'>{patrimoineValue.toFixed(2)} Ariary</span>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
