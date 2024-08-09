036920f152bd796c073ba336bd8e566bec85c0e5
Ice_dera3 <hei.sedera@gmail.com>
2024-08-09 20:07:09 +0300

    feat: add data files

--- patrimoine/src/App.jsx
+++ patrimoine/src/App.jsx
@@ -1,12 +1,32 @@
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from 'react-bootstrap/Table';
+import './App.css';
+import AddInput from './components/affichage/AddInput';
 
 function App() {
-
-  return (
-    <>
-      
-    </>
-  )
+  return <>
+    <div className="container">
+      <div className='title-container'>
+        <h3>Patrimoine</h3>
+      </div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Libelle</th>
+            <th>Valeur Initial</th>
+            <th>Date Debut</th>
+            <th>Date Fin</th>
+            <th>Ammortisement</th>
+            <th>Valeur Actuelle</th>
+          </tr>
+        </thead>
+        <tbody>
+        <button className='add-btn' onClick={AddInput}>add</button>
+          
+        </tbody>
+      </Table>
+    </div>
+  </>
 }
 
-export default App
+export default App;
---
+++ patrimoine/src/components/affichage/AddInput.jsx
@@ -0,0 +1,12 @@
+export default function AddInput(){
+    return <>
+                 <tr>
+                    <td><input type="text" /></td>
+                    <td><input type="text" /></td>
+                    <td><input type="text" /></td>
+                    <td><input type="text" /></td>
+                    <td><input type="text" /></td>
+                    <td><input type="text" /></td>
+                </tr>
+    </>
+}
