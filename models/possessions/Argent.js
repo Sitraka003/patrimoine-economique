d98cb4ff40ec9b39a85ef9e18788513745ca4bbb
Phylosius <hei.phylosius@gmail.com>
2024-08-10 12:24:14 +0300

    feat: added JSDoc

--- models/possessions/Argent.js
+++ models/possessions/Argent.js
@@ -1,11 +1,27 @@
 import Possession from "./Possession.js";
+
+/**
+ * Type d'Argent possible
+ * */
 var TYPE_ARGENT = {
   Courant: "Courant",
   Epargne: "Epargne",
   Espece: "Espece"
 };
 
+/**
+ * Classe representant d'Argent (Valeur monétaire)
+ * */
 export default class Argent extends Possession {
+  /**
+   * @param possesseur {Personne} Personne possesseur de l'argent
+   * @param libelle {String} Petite description de l'argent
+   * @param valeur {number} Valeur de l'argent
+   * @param dateDebut {Date} Date d'obtention de l'argent
+   * @param dateFin {Date} Date de cloture de l'argent
+   * @param tauxAmortissement {int} Taux d'ammortissement de l'argent
+   * @param type {TYPE_ARGENT} Type de l'argent
+   * */
   constructor(possesseur, libelle, valeur, dateDebut, dateFin, tauxAmortissement, type) {
     super(possesseur, libelle, valeur, dateDebut, dateFin, tauxAmortissement);
     try {
--- models/possessions/BienMateriel.js
+++ models/possessions/BienMateriel.js
@@ -1,9 +1,28 @@
 import Possession from "./Possession.js";
+
+/**
+ * Classe representant un bien materiel (Possession physique)
+ * */
 export default class BienMateriel extends Possession {
+
+  /**
+   * @param possesseur {Personne} Personne possesseur du bien matériel
+   * @param libelle {String} Petite description du bien matériel
+   * @param valeur {number} Valeur du bien matériel
+   * @param dateDebut {Date} Date d'obtiention du bien matériel
+   * @param dateFin {Date} Date de cloture du bien matériel
+   * @param tauxAmortissement {number} Taux d'ammortissement du bien matériel
+   * */
   constructor(possesseur, libelle, valeur, dateDebut, dateFin, tauxAmortissement) {
     super(possesseur, libelle, valeur, dateDebut, dateFin, tauxAmortissement);
   }
 
+  /**
+   * Retourne la valeur du bien matériel à la date donnée
+   *
+   * @param date {Date} Date pour le calcul de la valeur du bien matériel
+   * @return {number} Valeur du bien matériel à la date donnée
+   * */
   getValeur(date) {
     super.getValeur(date);
   }
