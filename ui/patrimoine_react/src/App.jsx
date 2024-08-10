48705ff78fa6f6388bbcf636e4e0f8a261ab5e4e
AinaElie <elie.anjarafanomezana@gmail.com>
2024-08-10 18:31:10 +0300

    feat: style

--- ui/patrimoine_react/src/App.jsx
+++ ui/patrimoine_react/src/App.jsx
@@ -27,30 +27,18 @@ function App() {
             <th>Date de Debut</th>
             <th>Date de Fin</th>
             <th>Taux d'amortissement</th>
-            <th>Type d'argent</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className='border-1'>{data.possessions.map(element => (
-              <p className='py-2 border-2'>{element.libelle}</p>
-            ))}</td>
-            <td className='border-1'>{data.possessions.map(element => (
-              <p className='py-2 border-2'>{Math.abs(element.valeur) || Math.abs(element.valeurConstante)}</p>
-            ))}</td>
-            <td className='border-1'>{data.possessions.map(element => (
-              <p className='py-2 border-2'>{new Date(element.dateDebut).toISOString().split('T')[0]}</p>
-            ))}</td>
-            <td className='border-1'>{data.possessions.map(element => (
-              <p className='py-2 border-2'>{element.dateFin || "..."}</p>
-            ))}</td>
-            <td className='border-1'>{data.possessions.map(element => (
-              <p className='py-2 border-2'>{element.tauxAmortissement || 0}</p>
-            ))}</td>
-            <td className='border-1'>{data.possessions.map(element => (
-              <p className='py-2 border-2'>{element.type || ""}</p>
-            ))}</td>
-          </tr>
+          {data.possessions.map(element => (
+            <tr>
+              <td className='py-2 px-3'> {element.libelle} </td>
+              <td className='py-2 px-3'> {Math.abs(element.valeur) || Math.abs(element.valeurConstante)} </td>
+              <td className='py-2 px-3'> {new Date(element.dateDebut).toISOString().split('T')[0]} </td>
+              <td className='py-2 px-3'> {element.dateFin || "..."} </td>
+              <td className='py-2 px-3'> {element.tauxAmortissement || 0} </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
 
