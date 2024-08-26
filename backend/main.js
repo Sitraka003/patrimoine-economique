4564b2dd05db257bee6d859dab0b362753ed158f
Nonie Randriatsara <hei.oninaina@gmail.com>
2024-08-26 22:44:59 +0300

    test : backend configuration

--- backend/main.js
+++ backend/main.js
@@ -8,6 +8,23 @@ app.get("/possession",(async(request, response)=>{
 }))
 
 
+app.get("/patrimoine/:valeur", (async(request, response)=>{
+    const valeur = request.params.valeur
+    const data = await readFile("../UI/public/data.json")
+    const possessions =  data.data[1].data.possessions
+
+    const filterPossession =  ()=>{
+   return possessions.filter((valeur) => possessions.valeur <= valeur)
+   } 
+   response.send(filterPossession())
+}))
+
+
 app.listen(3000, () =>{
     console.log("je teste")
 })
+
+app.post("/newValue", (async(request, reponse)=>{
+    const body = request.body
+    request.send(body)
+}))
