c234839dc85fdfa919f80becee30524386551bd3
Ranto-Creat <hei.ranto.2@gmail.com>
2024-08-29 20:26:36 +0300

    feat: add a new fonctionality for express.json in server

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const personRoutes = require("./routes/personRoute");
+const personRoutes = require("./routes/Personroute");
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json());
 app.use("/api/persons", personRoutes);
 
 app.listen(PORT, () => {
