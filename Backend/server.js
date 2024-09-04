224a5f93bf7ef58b0e27c5f79c8356e9e71a7dac
Ranto-Creat <hei.ranto.2@gmail.com>
2024-09-04 20:04:57 +0300

    fix: update sever.js

--- Backend/server.js
+++ Backend/server.js
@@ -9,8 +9,7 @@ const PORT = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
-app.use("/api/persons", personRoutes);
-
+app.use("/api", personRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
