27832fede464043c916b0e30ee7977d04c242044
Ranto-Creat <hei.ranto.2@gmail.com>
2024-09-04 20:11:02 +0300

    refractor: modify the chart style

--- ui/src/components/chart/LineChart.jsx
+++ ui/src/components/chart/LineChart.jsx
@@ -1,3 +1,5 @@
+// src/components/ChartComponent.js
+
 import React from "react";
 import { Line } from "react-chartjs-2";
 import {
@@ -21,76 +23,15 @@ ChartJS.register(
   Legend
 );
 
-const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "top",
-    },
-    tooltip: {
-      mode: "index",
-      intersect: false,
-      backgroundColor: "rgba(0, 0, 0, 0.7)",
-      titleColor: "white",
-      bodyColor: "white",
-    },
-  },
-  hover: {
-    mode: "nearest",
-    intersect: true,
-  },
-  scales: {
-    x: {
-      display: true,
-      title: {
-        display: true,
-        text: "Libellés",
-      },
-    },
-    y: {
-      display: true,
-      title: {
-        display: true,
-        text: "Valeur",
-      },
-      beginAtZero: true,
-    },
-  },
-  animation: {
-    duration: 1000,
-    easing: "easeInOutQuad",
-  },
-};
-const LineChart = ({ data, options = {}, styles = {} }) => {
-  const prepareLineChartData = () => {
-    return {
-      labels: data.map((item) => item.libelle),
-      datasets: [
-        {
-          label: "Valeur Actuelle",
-          data: data.map((item) => item.valeur),
-          borderColor: styles.borderColor || "#4e73df",
-          backgroundColor: styles.backgroundColor || "rgba(78, 115, 223, 0.2)",
-          borderWidth: styles.borderWidth || 1,
-          fill: styles.fill || true,
-        },
-      ],
-    };
-  };
-
+const ChartComponent = ({ chartData }) => {
   return (
-    <div className="chart-container">
-      <h2 className="text-center">Graphique des Possessions (Lignes)</h2>
-      {data.length > 0 ? (
-        <Line data={prepareLineChartData()} options={options} />
-      ) : (
-        <p className="text-center">
-          Aucune donnée disponible pour afficher le graphique!
-        </p>
-      )}
+    <div className="mt-5 bg-white p-5 m-5 rounded-3">
+      <h2 className="text-center mb-4">
+        Graphique des Valeurs des Possessions
+      </h2>
+      <Line data={chartData} />
     </div>
   );
 };
 
-
-export default LineChart;
+export default ChartComponent;
