3b32bd7e3d07e2efc22d13c08a2aab5ebe774b64
Fanantenana Ny Aina <hei.fanantenana@gmail.com>
2024-09-06 20:01:29 +0300

    feat: add models folder with all the useful JS classes inside backend

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import cors from 'cors';
-import Flux from '../models/possessions/Flux.js';
-import Patrimoine from '../models/Patrimoine.js';
-import Possession from '../models/possessions/Possession.js';
+import Flux from './models/possessions/Flux.js';
+import Patrimoine from './models/Patrimoine.js';
+import Possession from './models/possessions/Possession.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
