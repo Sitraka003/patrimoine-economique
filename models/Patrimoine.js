eefcbb90f01aae46697362722652554b469dbb8d
Mihajatiana-dev <hei.mihajatiana@gmail.com>
2024-09-07 02:19:02 +0300

    fix: add flux on Patrimoine

--- models/Patrimoine.js
+++ models/Patrimoine.js
@@ -1,21 +1,31 @@
-import Possession from "./possessions/Possession.js";
+import Possession from "./Possession.js";
+import Flux from "./Flux.js";
 
 export default class Patrimoine {
   constructor(possesseur, possessions) {
     this.possesseur = possesseur;
-    this.possessions = possessions.map(
-      (p) =>
-        new Possession(
+    this.possessions = possessions.map((p) => {
+      if (p.jour !== undefined) {
+        return new Flux(
           p.possesseur,
           p.libelle,
           p.valeur,
           new Date(p.dateDebut),
           p.dateFin ? new Date(p.dateFin) : null,
           p.tauxAmortissement,
-          p.jour,
-          p.valeurConstante
-        )
-    );
+          p.jour
+        );
+      } else {
+        return new Possession(
+          p.possesseur,
+          p.libelle,
+          p.valeur,
+          new Date(p.dateDebut),
+          p.dateFin ? new Date(p.dateFin) : null,
+          p.tauxAmortissement
+        );
+      }
+    });
   }
 
   getValeur(date) {
