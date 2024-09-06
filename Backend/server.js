f3965a523a325c0bf465a003bbd01c4550c58e59
Ranto-Creat <hei.ranto.2@gmail.com>
2024-09-06 16:30:23 +0300

    feat: deployee project backend

--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const personRoutes = require("./routes/Personroute");
+require('dotenv').config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+const dataPath = process.env.DATA_FILE_PATH || '../data/data.json';
+
+console.log(`Chemin du fichier data : ${dataPath}`);
 
 app.use(cors());
 app.use(bodyParser.json());
