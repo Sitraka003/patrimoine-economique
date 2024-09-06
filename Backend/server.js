c2fd95acd6edeaeec5ff960369af72a387599dd0
Ranto-Creat <hei.ranto.2@gmail.com>
2024-09-06 22:50:14 +0300

    chore: desinstall dotenv

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const personRoutes = require("./routes/personRoute");
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
