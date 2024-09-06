114bb145834e3382b3f5c74d2a54cad67fadd8be
Mihajatiana-dev <hei.mihajatiana@gmail.com>
2024-09-07 02:47:01 +0300

    fix: test endpoint

--- models/Patrimoine.js
+++ models/Patrimoine.js
@@ -1,38 +1,50 @@
-import Possession from "./possessions/Possession.js";
+import Possession from "./possessions/Possession.js"; // Assurez-vous que le chemin est correct
 
 export default class Patrimoine {
   constructor(possesseur, possessions) {
     this.possesseur = possesseur;
-    this.possessions = possessions.map((possessionData) => {
-      if (possessionData.jour !== undefined) {
-        // Si c'est un flux
-        return new Flux(
-          possessionData.possesseur,
-          possessionData.libelle,
-          possessionData.valeurConstante,
-          new Date(possessionData.dateDebut),
-          possessionData.dateFin ? new Date(possessionData.dateFin) : null,
-          possessionData.tauxAmortissement,
-          possessionData.jour
-        );
-      } else {
-        // Si c'est une possession normale
-        return new Possession(
-          possessionData.possesseur,
-          possessionData.libelle,
-          possessionData.valeur,
-          new Date(possessionData.dateDebut),
-          possessionData.dateFin ? new Date(possessionData.dateFin) : null,
-          possessionData.tauxAmortissement
-        );
-      }
-    });
+    // Conversion des données en instances de Possession
+    this.possessions = possessions.map(
+      (p) =>
+        new Possession(
+          p.possesseur,
+          p.libelle,
+          p.valeur,
+          new Date(p.dateDebut),
+          p.dateFin ? new Date(p.dateFin) : null,
+          p.tauxAmortissement,
+          p.jour,
+          p.valeurConstante
+        )
+    );
   }
 
   getValeur(date) {
-    // Calculer la valeur totale du patrimoine à une date donnée
-    return this.possessions.reduce((total, possession) => {
-      return total + possession.getValeur(date);
-    }, 0);
+    let result = 0;
+    for (const item of this.possessions) {
+      result += item.getValeur(date);
+    }
+    return result;
+  }
+
+  addPossession(possession) {
+    if (possession.possesseur !== this.possesseur) {
+      console.log(
+        `${possession.libelle} n'appartient pas à ${this.possesseur}`
+      );
+    } else {
+      // Ajouter uniquement si ce n'est pas déjà dans la liste
+      if (!this.possessions.some((p) => p.libelle === possession.libelle)) {
+        this.possessions.push(possession);
+      } else {
+        console.log(`${possession.libelle} est déjà dans le patrimoine.`);
+      }
+    }
+  }
+
+  removePossession(possession) {
+    this.possessions = this.possessions.filter(
+      (p) => p.libelle !== possession.libelle
+    );
   }
 }
